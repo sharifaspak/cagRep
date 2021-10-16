c24a3686d36db08cc48e3f04d8ea549013b06506
sharifaspak <37010522+sharifaspak@users.noreply.github.com>
2021-10-16 09:16:13 +0400

    disable csrf

---
+++ EReportingCAG/src/main/java/com/cag/security/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.cag.security;
+
+/**
+ * @author aspak.avesh.sharif
+ *
+ */
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
+	}
+
+	/*
+	 * @Bean CorsConfigurationSource corsConfigurationSource() { CorsConfiguration
+	 * configuration = new CorsConfiguration();
+	 * configuration.setAllowedOrigins(Arrays.asList("*"));
+	 * configuration.setAllowedMethods(Arrays.asList("*"));
+	 * configuration.setAllowedHeaders(Arrays.asList("*"));
+	 * configuration.setAllowCredentials(true); UrlBasedCorsConfigurationSource
+	 * source = new UrlBasedCorsConfigurationSource();
+	 * source.registerCorsConfiguration("/**", configuration); return source; }
+	 */
+
+}
